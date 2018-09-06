141b6cf87ba88c64360812ad3fcd00554b9818ef
Anderson Henrique da Silva <anderson.h.silva@gmail.com>
2018-09-05 22:31:12 -0300

    criacao de login do usuario pelo dao

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -43,6 +43,7 @@ class Usuario {
     return $this;
   }
 
+// busca usuario no banco pelo id
   public function loadById($id){
 
       $sql = new Sql();
@@ -61,6 +62,38 @@ class Usuario {
 
   }
 
+  // lista os dados do banco
+  public static function getList(){
+    $sql = new Sql();
+    return $sql->select("SELECT * FROM aula1 ORDER BY id DESC; ");
+  }
+
+  // consulta os dados do banco
+  public static function search($condicao){
+    $sql = new Sql();
+    return $sql->select("SELECT * FROM aula1 WHERE nome LIKE :SEARCH; ", array(
+      ":SEARCH"=>$condicao
+    ));
+  }
+
+  // realiza login no banco
+  public static function login($usuario, $senha): string {
+    $sql = new Sql();
+    $result = $sql->select("SELECT * FROM aula1 WHERE nome = :USUARIO AND senha = :SENHA; ", array(
+      ":USUARIO"=>$usuario,
+      ":SENHA"=>$senha
+    ));
+
+    if (count($result) > 0){
+      $row = $result[0];
+      return $row['nome'];
+
+    } else {
+      throw new Exception("Usuario ou senha incorreto!");
+    }
+  }
+
+  // imprime o nome que eu quero pelo echo
   public function __toString(){
     return "Objeto Usuario com nome de: ".$this->getNome();
   }
--- DAO/index.php
+++ DAO/index.php
@@ -1,9 +1,17 @@
 <?php
 require_once("config.php");
 
-$usuario = new Usuario();
-$usuario->loadById(8);
-echo $usuario;
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//echo $usuario;
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//$lista = Usuario::search("%Henrique%");
+//echo json_encode($lista);
+
+echo Usuario::login("Anderson", "486486");
 
 
 ?>
