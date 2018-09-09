50a893ce17907911078cabf34ca0cfae8663f111
Anderson Henrique da Silva <anderson.h.silva@gmail.com>
2018-09-09 17:04:16 -0300

    criacao do thumbnail - tratamento de imagens com a lib GD do PHP7

--- lib_GD/certificado.php
+++ lib_GD/certificado.php
@@ -1,24 +1,28 @@
 <?php
 /*
     ATENÇÃO! Para que o codigo a baixo funciona tem que o php ter a biblioteca GD adicionada ao seu extension_dir do php.ini
+    este codigo só funciona se tambem se setar a permissao do dono do arquivo no usuario e grupo de permissao do apache2.conf
 */
+
 $image = imagecreatefromjpeg("certificado.jpg");
 $titlecolor = imagecolorallocate($image, 0, 0, 0);
 $gray = imagecolorallocate($image, 100, 100, 100);
+$font_bevan = "fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf";
+$font_playball = "fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf";
 
 $nome_aluno = "Anderson Henrique";
 
-imagestring($image, 5, 450, 150, "CERTIFICADO",$titlecolor);
-imagestring($image, 5, 440, 350, $nome_aluno, $titlecolor);
+// exibe texto com uma fonte personalizada
+imagettftext($image, 32, 0, 320, 250, $titlecolor, $font_bevan, "CERTIFICADO");
+imagettftext($image, 32, 0, 375, 350, $titlecolor, $font_playball, $nome_aluno);
+
 imagestring($image, 3, 440, 370, utf8_encode("Concluido em:").date("d/m/Y"),$titlecolor);
 
 //imagejpeg($image, "Certificado-".date("Y,m,d").".jpg", 10);
 header("Content-Type: image/jpeg");
 
-//mkdir('teste');
-
-//imagejpeg($image); // exibe imagem na tela
-imagejpeg($image, "certificado-".date("Y-m-d").".jpg", 60); // salva uma imagem local no servidor
+imagejpeg($image); // exibe imagem na tela
+//imagejpeg($image, "certificado-".date("Y-m-d").".jpg", 60); // salva uma imagem local no servidor
 
 // limpra a memoria do php
 imagedestroy($image);
--- lib_GD/miniatura_thumbnail.php
+++ lib_GD/miniatura_thumbnail.php
@@ -1,25 +1,29 @@
 <?php
 
-//header("Content-Type: image/png");
+header("Content-Type: image/jpeg");
 
 $file = "wallpaper.jpg";
 
-$new_width = 256;
-$new_height = 256;
-
+// pega as dimensoes atuais do arquivo
 $data = getimagesize($file);
 //var_dump($data);
+$old_width = $data[0];
+$old_height = $data[1];
 
-list($old_width, $old_height) = $data;
+// define as novas dimensoes do arquivo mantendo as proporcoes
+$new_width = (int) ($old_width * 0.15);
+$new_height = (int) ($old_height * 0.15);
 
 $new_image = imagecreatetruecolor($new_width, $new_height);
-$old_image = imagecreatetruecolor($file);
+$old_image = imagecreatefromjpeg($file);
 
+// eixo X 0,0 e Y 0,0
 imagecopyresampled($new_image, $old_image, 0, 0, 0, 0, $new_width, $new_height, $old_width, $old_height);
-imagejpeg($image, "certificado-".date("Y-m-d").".jpg",10);
 
-// destroi limpando a imagem da memoria do php
-imagedestroy($image);
+imagejpeg($new_image); // exibe imagem na tela
 
+// limpra a memoria do php
+imagedestroy($old_image);
+imagedestroy($new_image);
 
  ?>
